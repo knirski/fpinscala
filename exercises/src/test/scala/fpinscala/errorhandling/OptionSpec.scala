8c274b818b807a4b474fe713f5e203ad5ca49e30
Krzysztof Nirski <krzysztof.nirski+github@gmail.com>
2017-11-12 21:21:28 +0100

    Fix test package

--- exercises/src/test/scala/fpinscala/errorhandling/OptionSpec.scala
+++ exercises/src/test/scala/fpinscala/errorhandling/OptionSpec.scala
@@ -1,33 +1,30 @@
-package fpinscala.datastructures
+package fpinscala.errorhandling
 
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
 class OptionSpec extends FunSuite {
 
-  private def some[A](value: A) = fpinscala.errorhandling.Some(value)
-  private val none = fpinscala.errorhandling.None
-
   test("Exercise 4.1 - Option interface") {
-    some(42).map(_.toString) shouldBe some("42")
-    none.map(_.toString) shouldBe none
+    Some(42).map(_.toString) shouldBe Some("42")
+    None.map(_.toString) shouldBe None
 
-    some(42).getOrElse(3) shouldBe 42
-    none.getOrElse(3) shouldBe 3
+    Some(42).getOrElse(3) shouldBe 42
+    None.getOrElse(3) shouldBe 3
 
-    some(42).flatMap(x => some(x.toString)) shouldBe some("42")
-    some(42).flatMap(_ => none) shouldBe none
-    none.flatMap(x => some(x.toString)) shouldBe none
-    none.flatMap(_ => none) shouldBe none
+    Some(42).flatMap(x => Some(x.toString)) shouldBe Some("42")
+    Some(42).flatMap(_ => None) shouldBe None
+    None.flatMap(x => Some(x.toString)) shouldBe None
+    None.flatMap(_ => None) shouldBe None
 
-    some(42).orElse(some(3)) shouldBe some(42)
-    some(42).orElse(none) shouldBe some(42)
-    none.orElse(some(3)) shouldBe some(3)
-    none.orElse(none) shouldBe none
+    Some(42).orElse(Some(3)) shouldBe Some(42)
+    Some(42).orElse(None) shouldBe Some(42)
+    None.orElse(Some(3)) shouldBe Some(3)
+    None.orElse(None) shouldBe None
 
-    some(42).filter(_ == 42) shouldBe some(42)
-    some(42).filter(_ == 3) shouldBe none
-    none.filter(_ == 42) shouldBe none
+    Some(42).filter(_ == 42) shouldBe Some(42)
+    Some(42).filter(_ == 3) shouldBe None
+    None.filter(_ == 42) shouldBe None
   }
 
 }
